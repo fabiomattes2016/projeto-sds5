cdbadf63c708e0810e82bd29981fef7977994788
Fabio Mattes <fabio.mattes@benner.com.br>
2021-11-04 15:12:26 -0300

    Pagination

--- backend/src/main/java/com/desuperior/dsvendas/Services/SaleService.java
+++ backend/src/main/java/com/desuperior/dsvendas/Services/SaleService.java
@@ -1,22 +1,27 @@
 package com.desuperior.dsvendas.Services;
 
-import java.util.stream.Collectors;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.desuperior.dsvendas.dto.SaleDTO;
 import com.desuperior.dsvendas.entities.Sale;
 import com.desuperior.dsvendas.repositories.SaleRepository;
+import com.desuperior.dsvendas.repositories.SellerRepository;
 
 @Service
 public class SaleService {
 	@Autowired
 	private SaleRepository repository;
 	
+	@Autowired
+	private SellerRepository sellerRepository;
+	
+	@Transactional(readOnly = true)
 	public Page<SaleDTO> findAll(Pageable pageable) {
+		sellerRepository.findAll();
 		Page<Sale> result = repository.findAll(pageable);
 		return result.map(x -> new SaleDTO(x));
 	}
